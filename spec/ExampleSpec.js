c22116d45550fc969032cc9af88db1c18b942a95
Stacie Farmer <farmerwebdesign@gmail.com>
2014-05-20 21:58:35 -0600

    Finished the last 6 exercises with passing tests

--- spec/ExampleSpec.js
+++ spec/ExampleSpec.js
@@ -20,4 +20,47 @@ describe("TryingItOut", function() {
   it("should translate phrases into rovarspraket", function() {
     expect(tryingitout.translateIntoRovarspraket('this is fun')).toEqual('tothohisos isos fofunon');
   });
+
+    it("should sum all numbers in an array", function() {
+        var arrayOfNumbers = [1,2,3,4];
+        expect(tryingitout.sumOfNumbersInArray(arrayOfNumbers)).toEqual(10);
+    });
+
+    it("should multiply all numbers in an array", function() {
+        var arrayOfNumbers = [1,2,3,4];
+        expect(tryingitout.multiplyNumbersInArray(arrayOfNumbers)).toEqual(24);
+    });
+
+    it("should reverse all letters in a string", function() {
+        var string = 'jag testar';
+        expect(tryingitout.reverseLettersInString(string)).toEqual('ratset gaj');
+    });
+
+    it("should translate christmas card from english to swedish", function() {
+        var englishVersion = 'merry christmas and happy new year';
+        expect(tryingitout.englishToSwedishTranslation(englishVersion)).toEqual('god jul och gott nytt &134;r');
+    });
+
+    it("should return the length of the longest word", function() {
+        var arrayOfWords = ['test', 'testing', 'testing123', 'test1'];
+        expect(tryingitout.findLongestWord(arrayOfWords)).toEqual(10);
+    });
+
+    it("should return the words that are longer than the integer", function() {
+        var arrayOfWords = ['test', 'testingforthewin', 'testing1234', 'test1'];
+        var integer = 10;
+        var arrayOfWordsThatShouldPass = ['testingforthewin', 'testing1234'];
+        expect(tryingitout.filterLongWords(arrayOfWords, integer)).toEqual(arrayOfWordsThatShouldPass);
+    });
+
+    it("should return the frequency of each character in the string", function() {
+        var stringToPass = 'abbabcbdbabdbdbabababcbcbab';
+        var objectTheShouldBeReturned = {
+            a : 7,
+            b : 14,
+            c : 3,
+            d : 3
+        };
+        expect(tryingitout.charFreq(stringToPass)).toEqual(objectTheShouldBeReturned);
+    });
 });
--- src/Example.js
+++ src/Example.js
@@ -29,7 +29,6 @@ TryingItOut.prototype.isAVowel = function(inputCharacter){
 TryingItOut.prototype.translateIntoRovarspraket = function(originalPhrase){
 	var finalPhrase = '';
 	for (var i = 0; i < originalPhrase.length; i++) {
-    	console.log(originalPhrase.charAt(i));
 		var isAVowel = 0;
 		var vowels = ['a','e','i','o','u',' '];
 		for(var x in vowels){
@@ -45,4 +44,83 @@ TryingItOut.prototype.translateIntoRovarspraket = function(originalPhrase){
 		}
 	}
 	return finalPhrase;
+};
+
+TryingItOut.prototype.sumOfNumbersInArray = function(arrayOfNumbers){
+    var sumOfNumbers = 0;
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+        sumOfNumbers += arrayOfNumbers[i];
+    }
+    return sumOfNumbers;
+};
+
+TryingItOut.prototype.multiplyNumbersInArray = function(arrayOfNumbers){
+    var multiplicationOfNumbers = 1;
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+        multiplicationOfNumbers = multiplicationOfNumbers * arrayOfNumbers[i];
+    }
+    return multiplicationOfNumbers;
+};
+
+TryingItOut.prototype.reverseLettersInString = function(initialString){
+    var reversedString = '';
+    for (var i = 1; i <= initialString.length; i++) {
+        var currentPositionFromEnd = initialString.length - i;
+        reversedString += initialString[currentPositionFromEnd];
+    }
+    return reversedString;
+};
+
+TryingItOut.prototype.englishToSwedishTranslation = function(englishString){
+    var swedishWords = {
+        "merry" : "god",
+        "christmas" : "jul",
+        "and" : "och",
+        "happy" : "gott",
+        "new" : "nytt",
+        "year" : "&134;r"
+    };
+    var swedishString = '';
+    var englishArray = englishString.split(" ");
+    for (var i = 0; i < englishArray.length; i++) {
+        var englishWord = englishArray[i];
+        swedishString += swedishWords[englishWord];
+        if(!(i === englishArray.length - 1)){
+            swedishString += " ";
+        }
+    }
+    return swedishString;
+};
+
+TryingItOut.prototype.findLongestWord = function(arrayOfWords){
+    var currentLongestNumber = 0;
+    for (var i = 0; i < arrayOfWords.length; i++) {
+        if(arrayOfWords[i].length > currentLongestNumber){
+            currentLongestNumber = arrayOfWords[i].length;
+        }
+    }
+    return currentLongestNumber;
+};
+
+TryingItOut.prototype.filterLongWords = function(arrayOfWords, integer){
+    var arrayOfWordsThatPass = [];
+    for (var i = 0; i < arrayOfWords.length; i++) {
+        if(arrayOfWords[i].length > integer){
+            arrayOfWordsThatPass.push(arrayOfWords[i]);
+        }
+    }
+    return arrayOfWordsThatPass;
+};
+
+TryingItOut.prototype.charFreq = function(stringGiven){
+    var frequencyObjects = {};
+    var arrayOfAlphabetChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    for (var i = 0; i < arrayOfAlphabetChars.length; i++){
+        if(stringGiven.indexOf(arrayOfAlphabetChars[i]) >= 0 && typeof(stringGiven.indexOf(arrayOfAlphabetChars[i])) === 'number'){
+            var characterRegEx = new RegExp( arrayOfAlphabetChars[i], 'g' );
+            var numberOfOccurrences = stringGiven.match(characterRegEx).length;
+            frequencyObjects[arrayOfAlphabetChars[i]] = numberOfOccurrences;
+        }
+    }
+    return frequencyObjects;
 };
